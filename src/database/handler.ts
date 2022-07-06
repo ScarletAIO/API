852551c5c7cc7da8a3a265bf9b508cd723bdd108
bits0rcerer <25325997+bits0rcerer@users.noreply.github.com>
2022-07-06 13:50:40 +0200

    prevent sql injection attacks

--- src/database/handler.ts
+++ src/database/handler.ts
@@ -1,7 +1,7 @@
 /** @format */
 
 require("dotenv").config();
-import  mysql from "mysql";
+import mysql from "mysql";
 import fs from "node:fs/promises";
 import crypto from "crypto";
 import path from "node:path";
@@ -26,7 +26,7 @@ export default class DataHandler {
 		const connection: any = await this.createConnection();
 		const sql: Promise<string> = fs.readFile(
 			path.join(__dirname, file),
-			"utf8"
+			"utf8",
 		);
 		connection.query(sql, (err: any, result: any) => {
 			if (err) {
@@ -63,8 +63,8 @@ export default class DataHandler {
 		const connection: any = await this.createConnection();
 		const { age, name, password, email } = userData;
 		// append the user data to the user.table
-		const query: string = `INSERT INTO \`user.table\` (age, name, password, email) VALUES (${age}, '${name}', '${password}', '${email}');`;
-		const result: any = await connection.query(query);
+		const query: string = `INSERT INTO \`user.table\` (age, name, password, email) VALUES (?, ?, ?, ?);`;
+		const result: any = await connection.query(query, [age, name, password, email]);
 		connection.end();
 		return JSON.stringify(result);
 	}
@@ -73,8 +73,8 @@ export default class DataHandler {
 		const connection: any = await this.createConnection();
 		const { age, name, password, email } = userData;
 		// update the user data in the user.table
-		const query: string = `UPDATE \`user.table\` SET age = ${age}, name = '${name}', password = '${password}', email = '${email}' WHERE id = '${userData.id}';`;
-		const result: any = await connection.query(query);
+		const query: string = `UPDATE \`user.table\` SET age = ?, name = ?, password = ?, email = ? WHERE id = ?;`;
+		const result: any = await connection.query(query, age, name, password, email, userData.id);
 		connection.end();
 		return result;
 	}
@@ -82,8 +82,8 @@ export default class DataHandler {
 	async deleteUserFromTable(userData: any | {}): Promise<any> {
 		const connection: any = await this.createConnection();
 		// delete the user data from the user.table
-		const query: string = `DELETE FROM \`user.table\` WHERE id = '${userData.id}';`;
-		const result: any = await connection.query(query);
+		const query: string = `DELETE FROM \`user.table\` WHERE id = ?;`;
+		const result: any = await connection.query(query, [userData.id]);
 		connection.end();
 		return result;
 	}
@@ -91,8 +91,8 @@ export default class DataHandler {
 	async getUserFromTable(userData: any | {}): Promise<any> {
 		const connection: any = await this.createConnection();
 		// get the user data from the user.table
-		const query: string = `SELECT * FROM \`user.table\` WHERE id = '${userData.id}';`;
-		const result: any = await connection.query(query);
+		const query: string = `SELECT * FROM \`user.table\` WHERE id = ?;`;
+		const result: any = await connection.query(query, [userData.id]);
 		connection.end();
 		return result;
 	}
@@ -108,12 +108,12 @@ export default class DataHandler {
 
 	async getUserFromTableByParam(
 		userData: any | {},
-		param: string
+		param: string,
 	): Promise<any> {
 		const connection: any = await this.createConnection();
 		// get the user data from the user.table
-		const query: string = `SELECT * FROM \`user.table\` WHERE ${param} = '${userData[param]}';`;
-		const result: any = await connection.query(query);
+		const query: string = `SELECT * FROM \`user.table\` WHERE ? = ?;`;
+		const result: any = await connection.query(query, [param, userData[param]]);
 		connection.end();
 		return result;
 	}
@@ -131,21 +131,22 @@ export default class DataHandler {
 			(err, key) => {
 				if (err) throw err;
 				const iv = crypto.randomBytes(16).toString("hex").slice(0, 16);
+				// TODO: do not forget to save the iv for decryption
 				const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
 				let encrypted = cipher.update(result, "utf8", "hex");
 				encrypted += cipher.final("hex");
 				fs.writeFile(
 					path.resolve(
-						`logs/database/backups/user.${Date.now()}.sql`
+						`logs/database/backups/user.${Date.now()}.sql`,
 					),
 					encrypted,
 					{
 						flag: "a+",
-					}
+					},
 				).catch((err) => {
 					Logger.send(err, "CRITICAL", false);
 				});
-			}
+			},
 		);
 	}
 }
